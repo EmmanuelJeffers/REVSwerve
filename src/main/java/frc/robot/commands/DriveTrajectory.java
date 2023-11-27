59609f4d3e0aa75bec53b80044f12d9531a327cf
Supreza <100383264+HakimB2007@users.noreply.github.com>
2023-11-26 22:50:17 -0500

    small adjustments

--- src/main/java/frc/robot/commands/DriveTrajectory.java
+++ src/main/java/frc/robot/commands/DriveTrajectory.java
@@ -15,10 +15,8 @@ import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.math.trajectory.TrajectoryConfig;
 import edu.wpi.first.math.trajectory.TrajectoryGenerator;
-import edu.wpi.first.wpilibj2.command.ProfiledPIDCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
-import frc.robot.Constants;
 import frc.robot.Constants.AutoConstants;
 import frc.robot.Constants.DriveConstants;
 import frc.robot.subsystems.DriveSubsystem;
@@ -28,23 +26,16 @@ import frc.robot.subsystems.DriveSubsystem;
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class DriveTrajectory extends SequentialCommandGroup {
   DriveSubsystem m_robotDrive = new DriveSubsystem();
-  /** Creates a new DriveTrajectory. */
   public DriveTrajectory() {
-    // Add your commands in the addCommands() call, e.g.
-    // addCommands(new FooCommand(), new BarCommand());
+    //
     TrajectoryConfig config = new TrajectoryConfig(
         AutoConstants.kMaxSpeedMetersPerSecond,
         AutoConstants.kMaxAccelerationMetersPerSecondSquared)
-        // Add kinematics to ensure max speed is actually obeyed
         .setKinematics(DriveConstants.kDriveKinematics);
 
-    // An example trajectory to follow. All units in meters.
     Trajectory trajectory = TrajectoryGenerator.generateTrajectory(
-        // Start at the origin facing the +X direction
-        new Pose2d(0, 0, new Rotation2d()),
-        // Pass through these two interior waypoints, making an 's' curve path
+        new Pose2d(0, 0, new Rotation2d(0)),
         List.of(new Translation2d(1, 0), new Translation2d(2, 0)),
-        // End 3 meters straight ahead of where we started, facing forward
         new Pose2d(3, 0, new Rotation2d(0)),
         config);
 
@@ -54,20 +45,16 @@ public class DriveTrajectory extends SequentialCommandGroup {
 
     SwerveControllerCommand swerveControllerCommand = new SwerveControllerCommand(
         trajectory,
-        m_robotDrive::getPose, // Functional interface to feed supplier
+        m_robotDrive::getPose, 
         DriveConstants.kDriveKinematics,
 
-        // Position controllers
         new PIDController(AutoConstants.kPXController, 0, 0),
         new PIDController(AutoConstants.kPYController, 0, 0),
         thetaController,
         m_robotDrive::setModuleStates,
         m_robotDrive);
 
-    // Reset odometry to the starting pose of the trajectory.
     m_robotDrive.resetOdometry(trajectory.getInitialPose());
-
-    // Run path following command, then stop at the end.
     
     addCommands();
   }
